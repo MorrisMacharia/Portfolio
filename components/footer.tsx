5d2a7ed7f7c8ff46348f0230d45b902a9584ed74
MorrisMacharia <mmmchiuri@gmail.com>
2024-07-22 13:58:26 +0300

    footer update

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,7 @@ export default function Footer() {
       <small className="mb-2 block text-xs">
         &copy; 2030 Morris. All rights reserved.
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
+      
     </footer>
   );
 }
